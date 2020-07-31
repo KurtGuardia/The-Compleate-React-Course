e49f3650c8752a8c4a1f6582fed0b6164f9fa9a0
Kurt Guardia <kurtguardia@gmail.com>
2020-07-31 11:06:28 +0200

    added the imprtant imports

---
+++ src/Components/Persons/Person/Person.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Aux from "../../../hoc/Aux";
+import classes from "./Person.css";
+import withClasses from "../../../hoc/withClass";
+import AuthContext from '../../../context/auth-context'
