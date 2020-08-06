bdfb01ef0f58048ab78b360a2a041e2c53bdf1a0
Kurt Guardia <kurtguardia@gmail.com>
2020-08-06 11:10:41 +0200

    created the JSX

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,3 +1,21 @@
 import React, { useEffect, useRef, useContext } from "react";
 import classes from "./Cockpit.css";
 import AuthContext from "../../context/auth-context";
+
+const Cockpit = (props) => {
+    
+  
+    console.log(props.personsLength);
+    return (
+      <div className={classes.Cockpit}>
+        <h1>{props.title}</h1>
+        <p className={assignedClasses.join(" ")}>this is really working!</p>
+        <button ref={toggleBtnRef} className={btnClasses} onClick={props.clicked}>
+          Toggle Persons
+        </button>
+        <AuthContext.Consumer>
+          {(context) => <button onClick={context.login}>Log in</button>}
+        </AuthContext.Consumer>
+      </div>
+    );
+  };
